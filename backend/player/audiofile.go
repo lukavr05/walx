0f682e7426b511599394f3958b61cbfc423e12d5
lukavr05 <lvrooyen@yahoo.com>
2025-06-02 15:30:36 +0100

    implemented basic AudioFile struct

--- backend/player/audiofile.go
+++ backend/player/audiofile.go
@@ -1,1 +1,19 @@
 package player
+
+type AudioFile struct {
+	name     string
+	path     string
+	duration float64
+}
+
+func (a *AudioFile) getName() string {
+	return a.name
+}
+
+func (a *AudioFile) getPath() string {
+	return a.path
+}
+
+func (a *AudioFile) getDuration() float64 {
+	return a.duration
+}
